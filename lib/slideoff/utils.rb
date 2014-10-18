6fcd3d1efcfee364d33abdff12003e33ef6ae5d9
DSIW <dsiw@dsiw-it.de>
2014-10-18 23:14:12 +0200

    Update example presentation

--- lib/slideoff/utils.rb
+++ lib/slideoff/utils.rb
@@ -9,33 +9,117 @@ module Slideoff
       FileUtils.mkdir_p name
       Dir.chdir(name) do |dir|
         FileUtils.mkdir_p 'main'
-        File.open('main/slides.md', 'w') do |file|
+        File.open('main/index.md', 'w') do |file|
           file.write <<-EOF
-!SLIDE title cover w
+!SLIDE title cover h
 #Title
 ##Subtitle
+![Image](https://farm4.staticflickr.com/3254/3034540524_2412428602_b.jpg)
+
+!SLIDE cover w
+![Image](https://farm4.staticflickr.com/3254/3034540524_2412428602_b.jpg)
+
+!SLIDE cover h
+![Image](https://farm4.staticflickr.com/3254/3034540524_2412428602_b.jpg)
+
+!SLIDE cover w h
+![Image](https://farm4.staticflickr.com/3254/3034540524_2412428602_b.jpg)
 
 !SLIDE
-# Content
+# Normal list
 
-* Introduction
-* Design
-* Implementation
-* Conclusion
-
-!SLIDE chapter
-# Design
-!F[3034540524]
-
-!SLIDE section
-# Architecture
-!F[5862134]
-
-!SLIDE
-# Architecture
-
-* Easy
+* Keep
+* It
+* Super
 * Simple
+
+!SLIDE chapter h
+# Chapter
+![Image](https://farm4.staticflickr.com/3254/3034540524_2412428602_b.jpg)
+
+!SLIDE section h
+# Section
+![Image](https://farm4.staticflickr.com/3254/3034540524_2412428602_b.jpg)
+
+!SLIDE shout up
+# Shout up!
+
+!SLIDE shout left
+# Shout left!
+
+!SLIDE shout right
+# Shout right!
+
+!SLIDE shout down
+# Shout down!
+
+!SLIDE
+# Bullet list
+
+* Keep
+* It
+* Super
+* Simple
+
+!SLIDE
+# Numbered list
+
+1. Keep
+1. It
+1. Super
+1. Simple
+
+!SLIDE small
+# Small slide with full of text
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.
+
+!SLIDE
+# Description list
+
+Ruby
+  : Loving scripting language
+HTTP
+  : Hyper Text Transfer Protocol
+CSS
+  : Cascading Style Sheet
+REST
+  : Representational state transfer
+
+!SLIDE
+# Link
+
+1. Click on [DSIW/slideoff](http://github.com/DSIW/slideoff).
+
+!SLIDE
+# Code
+
+```sh
+$ gem install slideoff
+```
+
+```ruby
+class World
+  def hello
+    puts "Hello \#{self.class.name}!"
+  end
+end
+
+World.new.hello #=> "Hello World!"
+```
+
+Note: Code `self.class.name` will be evaluated.
+
+!SLIDE
+# Table
+
+|Left column|Centered column| Right column
+|-----------|:-------------:|------------:
+|Dataset 1  |Dataset 1      | Dataset 2
+|Dataset 1  |Dataset 2      | Dataset 2
+|Dataset 1  |Dataset 3      | Dataset 2
+|Dataset 1  |Dataset 4      | Dataset 2
 
 !SLIDE
 # Colors
@@ -57,12 +141,44 @@ module Slideoff
 1. <mark class="blue">abc</mark>
 1. <mark class="bluegreen">abc</mark>
 
-!SLIDE incremental
+!SLIDE
+# Colorize with custom markdown
+
+This is ==orange==some== __orange__super__ and _underlined_ text.
+
+!SLIDE incr-list
 # Incremental list
 
 * First item
 * Second item
 * Third item
+
+!SLIDE incr-code
+# Incremental Code
+
+```ruby
+1.class #=> Fixnum
+1.1.class #=> Float
+"abc".clas #=> String
+:abc.clas #=> Symbol
+```
+
+!SLIDE typewriter
+# Incremental Code
+
+```sh
+bundle install
+```
+
+!SLIDE incr-table
+# Incremental table
+
+|Left column|Centered column| Right column
+|-----------|:-------------:|------------:
+|Dataset 1  |Dataset 1      | Dataset 2
+|Dataset 1  |Dataset 2      | Dataset 2
+|Dataset 1  |Dataset 3      | Dataset 2
+|Dataset 1  |Dataset 4      | Dataset 2
 
 !SLIDE incremental
 # Content with pauses
@@ -73,19 +189,13 @@ This is the first text.
 
 More text <span class="inactive">with hidden info</span>!
 
-!SLIDE
-# Description list
-
-ABC
-  : abc
-DEF
-  : def
+!SLIDE disabled
+# Hidden slide
 
 !SLIDE
 # Blockquote
 
->Alles klar hjo joasdkb askdjb kabsdk jbaskdjb kabjd kjasbdk jbask dbaks bdkasjbd kasjb
->askdjbaksdb aksjbd aksbd aksbdaksbd aksdb aksbdaksbd askbd aksdb aks baksb dak sbda
+>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
 >
 >Athor Name
 
@@ -101,7 +211,24 @@ DEF
 +++ Content
 
 !SLIDE
-# Box with different colors
+# Arranged Box
+
+<div class="col2">
+  <div class="box box-alert">
+    <div>Errors</div>
+    <div>aren't good</div>
+  </div>
+
+  <div class="inactive">
+    <div class="box box-success">
+      <div>No errors</div>
+      <div>are better</div>
+    </div>
+  </div>
+</div>
+
+!SLIDE
+# Box with different colors (1)
 
 +++ box-blue
 +++ Header
@@ -116,7 +243,7 @@ DEF
 +++ Content
 
 !SLIDE
-# Box with different colors
+# Box with different colors (2)
 
 +++ box-red
 +++ Header
@@ -133,12 +260,72 @@ DEF
 !SLIDE
 # Image
 
-![](http://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg)
+![Markdown](http://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg)
 
-!SLIDE shout up
-# JPO
+!SLIDE img-place-middle
+# Centered Image
 
-!SLIDE cover w
+![Markdown](http://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg)
+
+!SLIDE section cover w
+# Image via Flickr API
+
+!F[22565509]
+
+!SLIDE
+# Placement with "place t r b l"
+
+<span class="bg-red place">center</span>
+<span class="bg-red place t">top</span>
+<span class="bg-red place t l">top-left</span>
+<span class="bg-red place t r">top-right</span>
+<span class="bg-red place l">left</span>
+<span class="bg-red place r">right</span>
+<span class="bg-red place b">bottom</span>
+<span class="bg-red place b l">bottom-left</span>
+<span class="bg-red place b r">bottom-right</span>
+
+!SLIDE
+# Diagram with Highcharts
+
+<div id="diagram-pie" style="width: 850px; height: 565px;"></div>
+<script type="text/javascript">
+$(function () {
+  $('#diagram-pie').highcharts({
+    credits: { enabled: false },
+    plotOptions: {
+      pie: {
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          distance: 40,
+          style: {
+            fontSize: '23px',
+            color: 'black'
+          },
+          formatter: function() {
+            return '<b>'+ this.point.name +'</b> ('+this.point.year+'): '+ Math.round(this.percentage*10)/10.0 +'%';
+          }
+        }
+      }
+    },
+    series: [{
+      type: 'pie',
+      innerSize: '45%',
+      data: [
+        { name: 'Dataset 1', year: '1980', y: 1.3,  },
+        { name: 'Dataset 2', year: '1990', y: 0.3,  },
+        { name: 'Dataset 3', year: '2000', y: 10.0, },
+        { name: 'Dataset 4', year: '2010', y: 88.3, },
+        { name: 'Dataset 5', year: '2020', y: 0.1   }
+      ]
+    }]
+  });
+});
+</script>
+
+
+!SLIDE noheader cover h
 
 <div class="left-33">
   <p style="font: 500 46px/1 'Open Sans'" class="text-center">Header</p>
@@ -152,50 +339,37 @@ DEF
   </ul>
 </div>
 
-!F[5696337148]
+![Image](https://farm4.staticflickr.com/3254/3034540524_2412428602_b.jpg)
 
 !SLIDE
-# Link
+# Clickbindings
 
-1. [GitHub.com](http://github.com)
-
-!SLIDE
-# Code
-
-```sh
-$ bundle install
-```
-
-```ruby
-class World
-  def hello
-    puts "Hello World"
-  end
-end
-```
+Click                           | Action
+------------------------------- | -------------------------
+Left click                      | Goto next slide
+Right click                     | Goto previous slide
 
 !SLIDE
-# Table
+# Keybindings (1)
 
-|Left column|Centered column| Right column
-|-----------|:-------------:|------------:
-|Dataset 1  |Dataset 1      | Dataset 2
-|Dataset 1  |Dataset 2      | Dataset 2
-|Dataset 1  |Dataset 3      | Dataset 2
-|Dataset 1  |Dataset 4      | Dataset 2
-
-!SLIDE
-# Highlighting
-
-__red__colorize__
-This is ==orange==some== __orange__super__ and _underlined_ text.
+Key                             | Action
+------------------------------- | -------------------------
+F5 / Enter                      | Goto slide mode
+Esc                             | Goto list mode
+Home                            | Goto first slide
+End                             | Goto last slide
 
 !SLIDE
-# Box
+# Keybindings (1)
 
-+++ box-red shadow
-+++ Header
-+++ Content
+Key                             | Action
+------------------------------- | -------------------------
+Tab / Space                     | Goto next slide
+... with Shift                  | Goto previous slide
+PageUp / Up / Left / h / k      | Goto previous slide
+... with Shift                  | Goto previous chapter
+PageDown / Down / right / l / j | Goto next slide
+... with Shift                  | Goto next chapter
           EOF
         end
         File.open('presentation.json', 'w') do |file|
@@ -210,7 +384,7 @@ This is ==orange==some== __orange__super__ and _underlined_ text.
   //"remote_host": "...",
   //"remote_path": "...",
   "sections": {
-    "main": {"title": "Talk 2.0", "show_chapter": false, "show_toc": false}
+    "main": {"title": "Modern theme", "show_chapter": false, "show_toc": false}
   }
 }
           EOF
